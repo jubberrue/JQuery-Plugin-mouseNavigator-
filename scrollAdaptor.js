a136ea157180b820e69749a5a3f61dee6b920f20
Josiah <jo_b_there@hotmail.com>
2011-06-29 20:15:55 +0200

    vertical scrolling added

--- scrollAdaptor.js
+++ scrollAdaptor.js
@@ -15,8 +15,24 @@
 			 	var navInterval=false;
 			 	var centerPoint = {x:0,y:0};
 			 	var mouse;
+			 	var DIRECTION_HORIZONTAL = "horizontal";
+			 	var DIRECTION_VERTICAL = "vertical";
 
-			 	init();
+			 	contentContainer = $(opts.contentSelector);
+				
+				if(contentContainer.length == 0){
+					contentContainer = viewPort.children().first();
+				};
+				
+				viewPort
+					.bind("mousemove.scrollAdaptor", function(e){handleMouseMove(e);})
+					.bind("mouseleave.scrollAdaptor", function(e){handleMouseOut(e);})
+					.bind("mouseenter.scrollAdaptor", function(e){handleMouseOver(e);})
+					.bind("resize.scrollAdaptor", function(e){handleResize(e);});
+				
+				if(opts.objAccessProxy){	
+					opts.objAccessProxy.bringItemIntoView = function(index){bringItemIntoView(index);};
+				}
 				
 			 	
 			 	
@@ -67,37 +83,56 @@
 				};
 				
 				function isInsideDeadZone(){
-					/*@TODO add vertical component*/
-					return (Math.abs(mouse.pageX - centerPoint.x) < opts.deadZoneSpread)? true : false;
+					return (opts.direction == DIRECTION_HORIZONTAL)?
+							(Math.abs(mouse.pageX - centerPoint.x) < opts.deadZoneSpread)? true : false :
+							(Math.abs(mouse.pageY - centerPoint.y) < opts.deadZoneSpread)? true : false;
 				};
 				
 				function isEntireThumbContainerFitInViewPort(){
-					/*@TODO add vertical component*/
 					var lastItem = contentContainer.children().last();
-					
-					return ((lastItem.position().left + lastItem.innerWidth())<= viewPort.innerWidth());
+
+					return (opts.direction == DIRECTION_HORIZONTAL)?
+								((lastItem.position().left + lastItem.innerWidth())<= viewPort.innerWidth()):
+								((lastItem.position().top + lastItem.innerHeight())<= viewPort.innerHeight());
 				};
 				
 				
 				function scrollBackward (dist){
-					/*@TODO add vertical component*/
 					var lastThumb = contentContainer.children().last();
 					
-					if((lastThumb.position().left + lastThumb.innerWidth() - Math.abs(contentContainer.position().left) ) < viewPort.innerWidth()){
-						debug("cannot scroll more to the left");
-						return;
-					}		
+					if(opts.direction == DIRECTION_HORIZONTAL){
+						if((lastThumb.position().left + lastThumb.innerWidth() - Math.abs(contentContainer.position().left) ) < viewPort.innerWidth()){ 
+							debug("cannot scroll more backwards");
+							return;
+						}		
+						
+						contentContainer.css("left", (contentContainer.position().left - dist) + "px");
+					}else{
+						if((lastThumb.position().top + lastThumb.innerHeight() - Math.abs(contentContainer.position().top) ) < viewPort.innerHeight()){ 
+							debug("cannot scroll more backwards");
+							return;
+						}		
+						
+						contentContainer.css("top", (contentContainer.position().top - dist) + "px");
+					}
 					
-					contentContainer.css("left", (contentContainer.position().left - dist) + "px");
 				};
 				
 				function scrollForward (dist){
-					/*@TODO add vertical component*/
-					if(contentContainer.position().left > 0){
-						debug("cannot scroll more to the right");
-						return;
+					if(opts.direction == DIRECTION_HORIZONTAL){
+						if(contentContainer.position().left > 0){
+							debug("cannot scroll more fowards");
+							return;
+						}
+						contentContainer.css("left", contentContainer.position().left + dist + "px");
+					}else{
+						if(contentContainer.position().top > 0){
+							debug("cannot scroll more fowards");
+							return;
+						}
+						contentContainer.css("top", contentContainer.position().top + dist + "px");
 					}
-					contentContainer.css("left", contentContainer.position().left + dist + "px");
+					
 				};
 				
 				function handleMouseOut(e){
@@ -124,70 +159,83 @@
 				};
 				
 				function calculateDuration(e){
-					/*@TODO add vertical component*/
 					mouse = e;
-					var dist = Math.abs(mouse.pageX - centerPoint.x);
-					var halfContainer = viewPort.innerWidth()/2;
+					var dist;
+					var halfContainer;
+					
+					if(opts.direction == DIRECTION_HORIZONTAL){
+						dist = Math.abs(mouse.pageX - centerPoint.x);
+						halfContainer = viewPort.innerWidth()/2;
+					}else{
+						dist = Math.abs(mouse.pageY - centerPoint.y);
+						halfContainer = viewPort.innerHeight()/2;
+					}
 					return (opts.maxDur - opts.minDur)*((halfContainer - dist)/(halfContainer)) + opts.minDur;
 				};
 				
 				function isScrollBackward(){
-					/*@TODO add vertical component*/
-					return (mouse.pageX > centerPoint.x)? true:false;
+					return (opts.direction == DIRECTION_HORIZONTAL)?
+							(mouse.pageX > centerPoint.x)? true:false : 
+							(mouse.pageY > centerPoint.y)? true:false; 
 				};
-				
-
 				
 				function isWithInContainer(targetItem, container){
 					var target = targetItem;
 					
 					//assume no relationship between target and container
-					if(target.offset().left >= container.offset().left){
-						return (target.innerWidth() + target.offset().left <= (container.offset().left + container.innerWidth()))? true : false;
+					if(opts.direction == DIRECTION_HORIZONTAL){
+						if(target.offset().left >= container.offset().left){
+							return (target.innerWidth() + target.offset().left <= (container.offset().left + container.innerWidth()))? true : false;
+						}else{
+							return false;
+						}
 					}else{
-						return false;
+						if(target.offset().top >= container.offset().top){
+							return (target.innerHeight() + target.offset().top <= (container.offset().top + container.innerHeight()))? true : false;
+						}else{
+							return false;
+						}
 					}
+							
+					
 				};
 				
 				function bringItemIntoView(index){
+					//@TODO add vertical functionality
 					 var thumb = $(contentContainer.children()[index]);
 						
 					 if(thumb.length == 0) return;
 					
-					
 					 if(isWithInContainer(thumb, viewPort)){
 						 debug("index " + index + " is within the view port");
 					 }else{
-						 debug("inner width" + thumb.innerWidth());
-						 var thumbXDelta = thumb.offset().left - viewPort.offset().left;
-						 var newContentContainerLeft;
-						 if(thumbXDelta < 0){
-							 newContentContainerLeft =  contentContainer.position().left + Math.abs(thumbXDelta);
+						 if(opts.direction == DIRECTION_HORIZONTAL){
+							 debug("inner width" + thumb.innerWidth());
+							 var thumbXDelta = thumb.offset().left - viewPort.offset().left;
+							 var newContentContainerLeft;
+							 if(thumbXDelta < 0){
+								 newContentContainerLeft =  contentContainer.position().left + Math.abs(thumbXDelta);
+							 }else{
+								 newContentContainerLeft =  contentContainer.position().left - (thumbXDelta - viewPort.innerWidth() + thumb.innerWidth());
+							 }
+							
+							 contentContainer.animate({left: newContentContainerLeft + "px"}, opts.showItemSpeed);
 						 }else{
-							 newContentContainerLeft =  contentContainer.position().left - (thumbXDelta - viewPort.innerWidth() + thumb.innerWidth());
+							 debug("inner height" + thumb.innerHeight());
+							 var thumbYDelta = thumb.offset().top - viewPort.offset().top;
+							 var newContentContainerTop;
+							 if(thumbYDelta < 0){
+								 newContentContainerTop =  contentContainer.position().top + Math.abs(thumbYDelta);
+							 }else{
+								 newContentContainerTop =  contentContainer.position().top - (thumbYDelta - viewPort.innerHeight() + thumb.innerHeight());
+							 }
+							
+							 contentContainer.animate({top: newContentContainerTop + "px"}, opts.showItemSpeed);
 						 }
-						
-						 contentContainer.animate({left: newContentContainerLeft + "px"}, opts.showItemSpeed);
+						 
 					 }
+					 
 				};
-				
-				function init(){
-			 		contentContainer = $(opts.contentSelector);
-					
-					if(contentContainer.length == 0){
-						contentContainer = viewPort.children().first();
-					};
-					
-					viewPort
-						.bind("mousemove.scrollAdaptor", function(e){handleMouseMove(e);})
-						.bind("mouseleave.scrollAdaptor", function(e){handleMouseOut(e);})
-						.bind("mouseenter.scrollAdaptor", function(e){handleMouseOver(e);})
-						.bind("resize.scrollAdaptor", function(e){handleResize(e);});
-					
-					if(opts.objAccessProxy){	
-						opts.objAccessProxy.bringItemIntoView = function(index){bringItemIntoView(index);};
-					}
-			 	};
 				
 		 });
 	 };
@@ -195,10 +243,11 @@
 	 $.fn.scrollAdaptor.defaults = {
 			 	stepSize:10,
 			 	minDur:10,
-			 	maxDur:50,
+			 	maxDur:20,
 			 	deadZoneSpread:25,
-			 	contentSelector:'.ad-thumb-list',
+			 	contentSelector:false,
 			 	showItemSpeed:20,
+			 	direction:"horizontal",
 			 	objAccessProxy:false
 			 };
 	 
